7e15e64d0a2c50c40b748d23bf1d2e61317c2750
EC2 Default User <mktsmabusela@gmail.com>
2023-09-17 20:04:12 +0000

    revert

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000; 
 
-//testing
 
 // Define a route that responds with "Hello, World!" when accessed
 app.get('/', (req, res) => {
